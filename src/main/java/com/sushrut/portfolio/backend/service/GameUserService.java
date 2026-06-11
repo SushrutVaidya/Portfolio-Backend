044892c93793832700435f6d0a9e994d4daa2995
Sushrut Vaidya <sushrut_sachin_vaidya@apple.com>
2026-06-11 15:30:17 +0530

    Remove duplicate imports from GameUserService (B1)

--- src/main/java/com/sushrut/portfolio/backend/service/GameUserService.java
+++ src/main/java/com/sushrut/portfolio/backend/service/GameUserService.java
@@ -13,15 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.sushrut.portfolio.backend.entities.GameUser;
 import com.sushrut.portfolio.backend.model.PlayerCardRequest;
